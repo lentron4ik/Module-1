d929d15f8417c2f4291d62424345c6f97352a9e8
Dima <zubko5971@gmail.com>
2024-09-23 10:51:37 +0300

    задание 1-3

---
+++ zd2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите первое целое число:");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе целое число:");
+            int number2 = int.Parse(Console.ReadLine());
+
+            // Вычисляем сумму
+            int sum = number1 + number2;
+
+            // Вывод суммы чисель
+            Console.WriteLine($"Сумма чисел {number1} и {number2} = {sum}");
+        }
+    }
+}
---
+++ zd3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            string reversed = ReverseString(input);
+
+            Console.WriteLine($"Строка в обратном порядке: {reversed}");
+        }
+
+        // Метод для разворота строки
+        static string ReverseString(string s)
+        {
+            char[] charArray = s.ToCharArray(); // Преобразование строки в массив символов
+            Array.Reverse(charArray);           // Переворачиваем массив символов
+            return new string(charArray);       // Преобразование массива символов обратно в строку
+        }
+    }
+}
